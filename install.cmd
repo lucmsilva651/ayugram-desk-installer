8aa6583f40cf4379b77b0c9cd992292111492fdd
Lucas Gabriel <lucmsilva651@gmail.com>
2025-02-06 11:22:15 -0300

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -1,10 +1,43 @@
 @echo off
 chcp 65001 > nul
 
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------      
+
 echo Removing old installs of AyuGram...
 rmdir /q /s %LOCALAPPDATA%\AyuGram
 rmdir /q /s %LOCALAPPDATA%\NirCmd
 cls
+
+echo Adding exclusions from Windows Defender...
+powershell -nologo -noprofile -command "Add-MpPreference -ExclusionPath "$env:LOCALAPPDATA\AyuGram"
+powershell -nologo -noprofile -command "Add-MpPreference -ExclusionPath "$env:LOCALAPPDATA\NirCmd"
 
 echo Downloading and installing from GitHub...
 powershell -nologo -noprofile -command "curl https://github.com/AyuGram/AyuGramDesktop/releases/latest/download/AyuGram.zip -o %LOCALAPPDATA%\AyuGram.zip"
